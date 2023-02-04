f585d419fe4bfa91ccfbafb741e25e4be0d7bd57
Jalilov <javohirjaliloff@gmail.com>
2023-02-04 14:56:10 +0500

    credit card

--- card.py
+++ card.py
@@ -3,7 +3,7 @@ from randommer import Randommer
 
 
 class Card(Randommer):
-    def get_card(self, api_key: str, type=None) -> dict:
+    def get_card(self, api_key: str, type_card=None) -> dict:
         '''get card from randommer
         
         Args:
@@ -13,7 +13,16 @@ class Card(Randommer):
         Returns:
             dict: card data
         '''
-        pass
+        base_url = self.get_url()
+        payload = {
+            "type":type_card
+        }
+        headers = {
+            "X-Api-Key":api_key
+        }
+        url = f"{base_url}/api/Card"
+        r = requests.get(url,params=payload, headers=headers)
+        return r.json()
 
     def get_card_types(self, api_key: str) -> list:
         '''get cars types from randommer
@@ -24,4 +33,15 @@ class Card(Randommer):
         Returns:
             list: list of types
         '''
-        pass
+        base_url = self.get_url()
+        headers = {
+            "X-Api-Key":api_key
+        }
+        url = f"{base_url}/api/Card/Types"
+        r = requests.get(url, headers=headers)
+        return r.json()
+
+card = Card()
+key = "f1ab06cd2da14928a4f4299e85162d76"
+
+print(card.get_card_types(key))
