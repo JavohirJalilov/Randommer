29a9212e52205a883bd2683c724848a0d17b2cd6
NurbekSaitqulov03 <nurbek.saitqulov23@gmail.com>
2023-03-06 23:06:41 +0500

    social number

--- social-number.py
+++ social-number.py
@@ -12,4 +12,15 @@ class SocialNumber(Randommer):
         Returns:
             str: number as str
         '''
-        pass
+        url = self.get_url()
+        headers = {
+            "X-Api-Key": api_key
+        }
+        response = requests.get(url=f'{url}/api/SocialNumber', headers=headers)
+        if response.status_code == 200:
+            return response.json()
+        return False
+
+soc_num = SocialNumber()
+key = "f1ab06cd2da14928a4f4299e85162d76"
+print(soc_num.get_SocialNumber(key))
